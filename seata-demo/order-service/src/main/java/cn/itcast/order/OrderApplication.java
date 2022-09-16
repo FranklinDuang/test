e05941d916c41937e04c5b9dd81d550d1731ece2
Franklin <franklinduang@163.com>
2022-09-16 21:06:58 +0800

    seata-demo commit

---
+++ seata-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.order;
+
+import io.seata.spring.annotation.GlobalTransactional;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author 虎哥
+ */
+@MapperScan("cn.itcast.order.mapper")
+@EnableFeignClients
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
